137b893bf6eca218ac755b607be17458fc954e27
Akash Trivedi <akashtrivedi@Akashs-MacBook-Air.local>
2016-01-10 22:44:32 -0800

    v1 of stream scrubbing code ready for review

--- server/routes/streamRoute_TEMP_FILE.js
+++ server/routes/streamRoute_TEMP_FILE.js
@@ -1,41 +1,31 @@
-//////////////////TO INSERT INSIDE ROUTER.GET FUNCTION ON LINE 42 OF ROUTES.JS FILE//////////////////
+//////////////////MEANT TO INSERT INSIDE THE ELSE CLAUSE OF THE STREAM REQUEST//////////////////
 
-  twitterApiController.getTweets(req.params.category, 10, function(err, data) {
-    // ** Reject data that doesn't have a location ** !!! Need to do this
-    if (err) {
-      console.log('Error getting data from Twttier API');
-      throw new Error(err);
+      //create a tweetObject and scrubbedTweetObject variable; set tweetObject equal to data.
+      var tweetObject = data; 
+      var scrubbedTweetObject;
+      //if tweet object from stream has a 'truthy' geo or coordinates property... 
+      if(tweetObject['geo'] || tweetObject['coordinates']){
+        //then create a scrubbedTweetObject containing most salient info...
+        var scrubbedTweetObject = {
+           name: tweetObject.user['name'],
+           handle: tweetObject.user['screen_name'],
+           verified: tweetObject.user['verified'],
+           createdAt: tweetObject.user['created_at'],
+           description: tweetObject.user['description'], 
+           url: tweetObject.user['url'],
+           followers_count: tweetObject.user['followers_count'], 
+           friends_count: tweetObject.user['friends_count'],
+           timezone: tweetObject.user['time_zone'],
+           coordinates: tweetObject['coordinates'],
+           geo: tweetObject['geo'],
+           place: tweetObject['place'],
+           tweetText: tweetObject['text'],
+           tweetTime: tweetObject['created_at'],
+           retweet_count: tweetObject['retweet_count'],
+           favorite_count: tweetObject['favorite_count']
+        };
+      }
+      //once tweetObject has been scrubbed, send it back up to the client side!
+      res.json(scrubbedTweetObject);
 
-    } else {
-      //create an empty scrubbedTweetData array.
-      var scrubbedTweetData = [];
-      //loop through each tweetObject returned from the twitter API...
-      data.statuses.forEach(function(tweetObject){
-        //if tweet object has a 'truthy' time_zone or coordinates property... 
-        if(tweetObject['geo'] || tweetObject['coordinates']){
-          //then create a scrubbedTweetObject containing most salient info...
-          var scrubbedTweetObject = {
-             name: tweetObject.user['name'],
-             handle: tweetObject.user['screen_name'],
-             verified: tweetObject.user['verified'],
-             createdAt: tweetObject.user['created_at'],
-             description: tweetObject.user['description'], 
-             url: tweetObject.user['url'],
-             followers_count: tweetObject.user['followers_count'], 
-             friends_count: tweetObject.user['friends_count'],
-             timezone: tweetObject.user['time_zone'],
-             coordinates: tweetObject['coordinates'],
-             geo: tweetObject['geo'],
-             place: tweetObject['place'],
-             tweetText: tweetObject['text'],
-             retweet_count: tweetObject['retweet_count'],
-             favorite_count: tweetObject['favorite_count']
-          };
-          //and push the scrubbed object to scrubbedTweetData.
-          scrubbedTweetData.push(scrubbedTweetObject);
-        }
-      });
-      //once data has been scrubbed, send it back up to the client side!
-      res.json(scrubbedTweetData);
-    }
-  });
+
